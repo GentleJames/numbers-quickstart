011a0b10c44ab0c5c77159e25088e0cb215f60e3
GentleJames <Christoph-James.Geisberger@t-systems.com>
2019-08-28 13:04:06 +0200

    ja

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace numbers_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e + f) / g;
+            Console.WriteLine(h);
+        }
+    }
+}
